2c17941e757e9d06491e8837bbb8ed5bda8f86ef
Sarah <eno@umd.edu>
2017-01-23 19:48:31 -0500

    better

--- mass/make_massjobs_signal.py
+++ mass/make_massjobs_signal.py
@@ -1,12 +1,12 @@
-#medmass = {400,600, 800, 1000, 1500, 2000}
-#dpmass = {1, 2, 5, 10}
-#dplt = {0.001, 0.1, 1, 5, 150, 300}
+medmass = {400,600, 800, 1000, 1500, 2000}
+dpmass = {1, 2, 5, 10}
+dplt = {"0p001", "0p1", "1", "5", "150", "300"}
 
 import subprocess
 
-medmass = {800}
-dpmass = {1}
-dplt = {1}
+#medmass = [800]
+#dpmass = [1]
+#dplt = ["0p001","0p1"]
 
 
 exearea = "/data/users/eno/em6/CMSSW_7_6_3/src/EmergingJetAnalysis/"
@@ -15,27 +15,29 @@ thisarea = "/data/users/eno/DARKJOBS/"
 f = open("massjobs.sh",'w')
 
 
-
 for i in medmass:
   for j in dpmass:
     for k in dplt:
-      bname = "EmergingJets_mass_X_d_"+str(i)+"_mass_pi_d_"+str(j)+"_tau_pi_d_"+str(k)
+#      print "HAHAHAHAHA"
+      bname = "EmergingJets_mass_X_d_"+str(i)+"_mass_pi_d_"+str(j)+"_tau_pi_d_"+k
+      scname = "Xd"+str(i)+"masspid"+str(j)+"taupid"+k
       aname = "/mnt/hadoop/cms//store/user/abelloni/EmJetMC/AODSIM-v1/"+bname+"_TuneCUETP8M1_13TeV_pythia8Mod/AODSIM-v1/"
-      #print aname
-      command = "find "+aname+" -name aodsim_1.root -print"
-      #print command
+#      print aname
+#      command = "find "+aname+" -name aodsim_1.root -print | grep -FzZ '000/aodsim_1.root'"
+      command = "find "+aname+" -name aodsim_1.root -print | grep '000/aodsim_1.root'"
+#      command = "find "+aname+" -name aodsim_1.root -print"
+#      print command
       p = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
       (namek, err) = p.communicate();
-      #print name
+#      print "namek is "+namek
       namev = namek.replace("aodsim_1.root"+'\n',"")
-      print "name is "+namev
-      command2 = "mkdir /data/users/eno/outputQCD/"+bname
+#      print "namev is "+namev
+      command2 = "mkdir /data/users/eno/outputQCD/"+scname
       q = subprocess.Popen(command2,stdout=subprocess.PIPE, shell=True)
-      print namev
-      command3= "ls "+namev+"*.root > /data/users/eno/DARKJOBS/local_"+bname+".txt"
-      print command3
+      command3= "ls "+namev+"*.root > /data/users/eno/DARKJOBS/"+scname+".txt"
+#      print command3
       q = subprocess.Popen(command3,stdout=subprocess.PIPE, shell=True)
-      name = bname
+      name = scname
       hostarea = "/data/users/eno/outputQCD/"+name+"/"
       jdlfile = open("condor-jobs-"+name+".jdl","w")
       jdlfile.write("universe = vanilla"+'\n')
